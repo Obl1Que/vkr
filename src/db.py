05a4768b0e89ee9d3fe9a6057936d7526b52bcd2
Obl1Que <s.dezhin@mail.ru>
2023-12-14 23:06:40 +0300

    Заготовка под подключение

--- src/db.py
+++ src/db.py
@@ -1,5 +1,10 @@
-import sqlite3 as sq
+import pymysql
+import os
 
 
-def Connect():
-    pass
+def Connect(address: str, port: str, username: str, password: str, db_name: str):
+    try:
+        connection = pymysql.connect(host=address, port=int(port), user=username, password=password, database=db_name)
+        return connection
+    except:
+        return None
