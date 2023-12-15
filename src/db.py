14fcd552518e809dde30c6df38d99428a409e5d4
Obl1Que <s.dezhin@mail.ru>
2023-12-15 21:03:19 +0300

    Изменен return в функции пробного подключения

--- src/db.py
+++ src/db.py
@@ -5,6 +5,6 @@ import os
 def Connect(address: str, port: str, username: str, password: str, db_name: str):
     try:
         connection = pymysql.connect(host=address, port=int(port), user=username, password=password, database=db_name)
-        return connection
+        return True
     except:
-        return None
+        return False
